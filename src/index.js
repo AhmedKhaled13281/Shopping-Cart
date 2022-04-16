c64915bf7b7bb32706986b24c36daf80888e9047
AhmedKhaled13281 <ahmedrashad13281@yahoo.com>
2022-04-16 06:50:38 +0200

    shopping-cart

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './Components/App'
+import {BrowserRouter , Link} from 'react-router-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './Components/Reducers/index'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/font-awesome/css/font-awesome.min.css'
+
+const store = createStore(reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <BrowserRouter>
+    <Provider store={store}>
+            <App />
+    </Provider>
+  </BrowserRouter>
+  , document.getElementById('root'))
